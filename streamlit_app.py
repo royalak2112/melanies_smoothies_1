2e58f5e48eca5794f53431c32158ddd162c1580a
royalak2112 <akashdawale96@gmail.com>
2024-10-03 13:39:00 +0530

    Update streamlit_app.py
    Abcw

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
     """
 )
 
-import streamlit as st
+from snowflake.snowpark.functions import col
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The Name on your smoothie will be:",name_on_order)
